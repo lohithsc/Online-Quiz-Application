474d936b0759a47fdbff0fe013beb4a1d66347b5
lohithsc <lohithsc31@gmail.com>
2026-06-19 13:13:58 +0530

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -1,1 +1,38 @@
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
 
+function displayQuestion() {
+  const questionData = quizData[currentQuestion];
+
+  const questionElement = document.createElement('div');
+  questionElement.className = 'question';
+  questionElement.innerHTML = questionData.question;
+
+  const optionsElement = document.createElement('div');
+  optionsElement.className = 'options';
+
+  const shuffledOptions = [...questionData.options];
+  shuffleArray(shuffledOptions);
+
+  shuffledOptions.forEach(optionText => {
+    const option = document.createElement('label');
+    option.className = 'option';
+
+    const radio = document.createElement('input');
+    radio.type = 'radio';
+    radio.name = 'quiz';
+    radio.value = optionText;
+
+    option.appendChild(radio);
+    option.appendChild(document.createTextNode(optionText));
+    optionsElement.appendChild(option);
+  });
+
+  quizContainer.innerHTML = '';
+  quizContainer.appendChild(questionElement);
+  quizContainer.appendChild(optionsElement);
+}
