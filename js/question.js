07e110355172dc73a6d272034bc96c345158c150
lohithsc <lohithsc31@gmail.com>
2026-06-19 13:13:21 +0530

    Update question.js

--- js/question.js
+++ js/question.js
@@ -1,1 +1,73 @@
-
+const quizData = [
+    {
+      
+       question: 'What is the capital of india?',
+      options: ['bengaluru', 'delhi', 'chennai', 'mumbai'],
+      answer: 'delhi',
+    },
+    {
+      question: 'What is the largest planet in our solar system?',
+      options: ['Mars', 'Saturn', 'Jupiter', 'Neptune'],
+      answer: 'Jupiter',
+    },
+    {
+      question: 'Which country won the t20 world cup in 2024?',
+      options: ['india', 'australia', 'south africa', 'new zealand'],
+      answer: 'india',
+    },
+    {
+      question: 'What is the tallest mountain in the world?',
+      options: ['Mount Everest', 'K2', 'Kangchenjunga', 'Makalu'],
+      answer: 'Mount Everest',
+    },
+    {
+      question: 'Which is the largest ocean on Earth?',
+      options: [
+        'Pacific Ocean',
+        'Indian Ocean',
+        'Atlantic Ocean',
+        'Arctic Ocean',
+      ],
+      answer: 'Pacific Ocean',
+    },
+    {
+      question: 'What is the chemical symbol for iron?',
+      options: ['Au', 'Ag', 'Cu', 'Fe'],
+      answer: 'Fe',
+    },
+    {
+      question: 'Who painted the Mona Lisa?',
+      options: [
+        'Pablo Picasso',
+        'Vincent van Gogh',
+        'Leonardo da Vinci',
+        'Michelangelo',
+      ],
+      answer: 'Leonardo da Vinci',
+    },
+    {
+      question: 'Which planet is known as the Red Planet?',
+      options: ['Mars', 'Venus', 'Mercury', 'Uranus'],
+      answer: 'Mars',
+    },
+    {
+      question: 'Which  is the largest country  in the world?',
+      options: [
+        'china',
+        'australia',
+        'canada',
+        'russia',
+      ],
+      answer: 'russia',
+    },
+    {
+      question: 'Which animal is known as the King of the Jungle?',
+      options: ['Lion', 'Tiger', 'Elephant', 'Giraffe'],
+      answer: 'Lion',
+    },
+    {
+      question:'national animal of india  ',
+      options:['tiger','lion','elephant','penguin'],
+      answer:'tiger',
+    }
+  ];
